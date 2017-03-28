da2f0629603eefae9c561c52178400d1f109322b
robertzml <robertzml@126.com>
2017-03-28 19:24:53 +0800

    Add multi-thread test to reading. Add AlarmReport controller. Remove some unused theme files.
    Signed-off-by: robertzml <robertzml@126.com>

--- Hyperion.UnitTest/AlarmReportTest.cs
+++ Hyperion.UnitTest/AlarmReportTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Hyperion.UnitTest
@@ -41,6 +43,61 @@ namespace Hyperion.UnitTest
         {
             var data = BusinessFactory<AlarmReportBusiness>.Instance.FindWithPage(10, 20).ToList();
             Assert.AreEqual(20, data.Count);
+        }
+
+        [TestMethod]
+        public void TestThread()
+        {
+            Task<int> task = new Task<int>(() =>
+            {
+                var data = BusinessFactory<AlarmReportBusiness>.Instance.FindAll();
+                return data.Count();
+            });
+
+            task.Start();
+            Console.WriteLine("Start");
+
+            task.Wait();
+            var result = task.Result;
+            Console.WriteLine(result.ToString());
+
+            //task.Wait();
+            //Console.WriteLine("result:" + task.Result.ToString());
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Thread thread = new Thread(FindAll);
+            //    thread.Start();
+            //    Console.WriteLine("loop " + i.ToString());
+            //}
+
+            Assert.AreEqual(56, result);
+            Console.WriteLine("FindAll");
+        }
+
+        [TestMethod]
+        public void TestThread2()
+        {
+            Console.WriteLine("test start");
+            var result = FindAll();
+            Console.WriteLine(result.Result.ToString());
+
+            Assert.IsTrue(true);
+            Console.WriteLine("test end");
+        }
+
+        private async Task<int> FindAll()
+        {
+            Task<int> task = new Task<int>(() =>
+            {
+                var data = BusinessFactory<AlarmReportBusiness>.Instance.FindAll();
+                return data.Count();
+            });
+
+            task.Start();
+
+            Console.WriteLine("task is running");
+            //var result = await task;
+            return await task;
         }
     }
 }
