4a186185e597181905dea3af00e70016c4d897c6
robertzml <robertzml@126.com>
2017-04-15 16:58:00 +0800

    Test use vue.js.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Hyperion.WebUI/Api/AlarmReportController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Hyperion.WebUI.Api
+{
+    using Poseidon.Base.Framework;
+    using Hyperion.Caller.Facade;
+    using Hyperion.Core.DL;
+
+    /// <summary>
+    /// 故障维护控制器
+    /// </summary>
+    public class AlarmReportController : ApiController
+    {
+        #region Action
+        /// <summary>
+        /// 获取所有故障维护
+        /// </summary>
+        /// <returns></returns>
+        public IHttpActionResult Get()
+        {
+            var data = CallerFactory<IAlarmReportService>.Instance.FindAll();
+            return Ok(data);
+        }
+
+        /// <summary>
+        /// 获取故障维护
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IHttpActionResult Get(int id)
+        {
+            var data = CallerFactory<IAlarmReportService>.Instance.FindById(id);
+            if (data == null)
+                return NotFound();
+
+            return Ok(data);
+        }
+        #endregion //Action
+    }
+}
