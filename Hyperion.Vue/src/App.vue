14dec5a2f59703aac4f2d4541c02d35e8bdf53a8
robertzml <robertzml@126.com>
2017-04-22 16:00:05 +0800

    add head, navmenu components.
    Signed-off-by: robertzml <robertzml@126.com>

--- Hyperion.Vue/src/App.vue
+++ Hyperion.Vue/src/App.vue
@@ -1,23 +1,41 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <top-head></top-head>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-2 col-sm-3 sidebar">
+          <nav-menu></nav-menu>
+        </div>          
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+
 <script>
+import 'bootstrap/dist/css/bootstrap.css'
+import './assets/css/hyperion.css'
+import $ from 'jquery'
+import TopHead from './components/Head'
+import NavMenu from './components/NavMenu'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    TopHead,
+    NavMenu
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
