6f5fa2959baa429fab1a1175f3720c4d7f40652a
robertzml <robertzml@126.com>
2017-06-20 19:53:29 +0800

    Add multi-thread test to read equipment.
    Signed-off-by: robertzml <robertzml@126.com>

--- Hyperion.Caller/WebApiCaller/EquipmentService.cs
+++ Hyperion.Caller/WebApiCaller/EquipmentService.cs
@@ -1,16 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Hyperion.Caller.WebApiCaller
 {
-    using Poseidon.Base.Framework;
     using Hyperion.Caller.Facade;
     using Hyperion.Core.DL;
+    using Poseidon.Base.Framework;
 
-    internal class EquipmentService
+    internal class EquipmentService : IEquipmentService
     {
+        private string host = "http://localhost:6024/api/";
+
+        public long Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Count<Tvalue>(string field, Tvalue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Equipment Create(Equipment entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Equipment Create(Equipment entity, bool generateKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Equipment entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Equipment> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Equipment> FindAllNormal()
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Method
+        public async Task<Equipment> FindById(long id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                
+                Equipment equipment = null;
+                string url = host + "equipment/" + id.ToString();
+
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    equipment = await response.Content.ReadAsAsync<Equipment>();
+                }
+
+                return equipment;
+            }
+        }
+
+        public IEnumerable<Equipment> FindListByField<Tvalue>(string field, Tvalue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Equipment FindOneByField<Tvalue>(string field, Tvalue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Equipment entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        Equipment IBaseService<Equipment, long>.FindById(long id)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion //Method
     }
 }
--- Hyperion.UnitTest/EquipmentTest.cs
+++ Hyperion.UnitTest/EquipmentTest.cs
@@ -1,23 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace Hyperion.UnitTest
 {
     using Xunit;
+    using Xunit.Abstractions;
     using Poseidon.Base.Framework;
     using Poseidon.Base.System;
     using Poseidon.Common;
+    using Hyperion.Caller.Facade;
     using Hyperion.Core.BL;
     using Hyperion.Core.DL;
 
     public class EquipmentTest
     {
-        public EquipmentTest()
+        private readonly ITestOutputHelper output;
+
+
+        public EquipmentTest(ITestOutputHelper output)
         {
+            this.output = output;
             GlobalAction.Initialize();
+        }
+
+        public async Task<Equipment> FindById(long id)
+        {
+            string host = "http://localhost:6024/api/";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                Equipment equipment = null;
+                string url = host + "equipment/" + id.ToString();
+
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    equipment = await response.Content.ReadAsAsync<Equipment>();
+                }
+
+                return equipment;
+            }
         }
 
         [Fact]
@@ -44,5 +76,96 @@ namespace Hyperion.UnitTest
 
             Assert.Equal(1, data.Count());
         }
+
+        [Fact]
+        public void TestCreateBatch()
+        {
+            int index = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                Equipment entity = new Equipment();
+                entity.SerialNumber = "abcedfg" + i.ToString();
+                entity.Vendor = i % 3 == 0 ? "Mulan" : "Aupu";
+                entity.CreateTime = DateTime.Now;
+                entity.UpdateTime = entity.CreateTime;
+                entity.Online = 1;
+                entity.Status = 0;
+
+                var result = BusinessFactory<EquipmentBusiness>.Instance.Create(entity);
+                index = i;
+            }
+
+            Assert.Equal(9999, index);
+        }
+
+        [Fact]
+        public void TestFindInThread2()
+        {
+            List<Task> tasks = new List<Task>();
+
+
+            Stopwatch total = new Stopwatch();
+
+            total.Start();
+            for (int i = 1; i < 10; i++)
+            {
+                var task = Task.Run(() =>
+                {
+                    Stopwatch st = new Stopwatch();
+                    st.Start();
+
+                    BusinessFactory<EquipmentBusiness>.Instance.FindById(i);
+
+                    st.Stop();
+
+                    output.WriteLine(string.Format("find time id is {0}, in {1} milliseconds", i, st.ElapsedMilliseconds));
+                });
+
+                tasks.Add(task);
+            }
+            Task.WaitAll(tasks.ToArray());
+
+            total.Stop();
+            output.WriteLine(string.Format("total time is {0} millisecond", total.ElapsedMilliseconds));
+
+            Assert.InRange(total.ElapsedMilliseconds, 0, 500000);
+        }
+
+
+        [Fact]
+        public void TestFindInThread()
+        {
+            List<Task> tasks = new List<Task>();
+            
+
+            Stopwatch total = new Stopwatch();
+
+            total.Start();
+            for (int i = 1; i < 10; i++)
+            {
+                var task = Task.Run(() =>
+                {
+                    Stopwatch st = new Stopwatch();
+                    st.Start();
+
+                    var t = FindById(i);
+
+                    var equipment = t.Result;
+
+                    st.Stop();
+
+                    output.WriteLine(string.Format("find time id is {0}, in {1} milliseconds", i, st.ElapsedMilliseconds));
+                });
+
+                tasks.Add(task);
+            }
+            Task.WaitAll(tasks.ToArray());
+
+            total.Stop();
+            output.WriteLine(string.Format("total time is {0} millisecond", total.ElapsedMilliseconds));
+
+            Assert.InRange(total.ElapsedMilliseconds, 0, 500000);
+        }
+        
     }
 }
